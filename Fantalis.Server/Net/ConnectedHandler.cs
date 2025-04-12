bee34a8053654e971fc0b5ddfb5a158daf78ff76
Dylan Renwick <skidsirongenesisdev@gmail.com>
2025-04-11 23:21:25 -0400

    Implemented ConnectedHandler

--- Fantalis.Server/Net/ConnectedHandler.cs
+++ Fantalis.Server/Net/ConnectedHandler.cs
@@ -9,9 +9,25 @@ public class ConnectedHandler : ConnectionDataHandler
     public ConnectedHandler(Connection connection, Logger logger)
         : base(connection, logger) { }
 
-
-    public override async Task HandleData(byte[] buffer, int bytesRead)
+    public override async Task<ConnectionState> HandleData(byte[] buffer, int bytesRead)
     {
-        
+        byte[] protocolVersion = Connection.Server.ProtocolVersion;
+        if (bytesRead != protocolVersion.Length)
+        {
+            Logger.Log("Protocol version length mismatch");
+            return ConnectionState.Disconnected;
+        }
+
+        for (int i = 0; i < bytesRead; i++)
+        {
+            if (buffer[i] != protocolVersion[i])
+            {
+                Logger.Log($"Protocol version mismatch at index {i}");
+                return ConnectionState.Disconnected;
+            }
+        }
+
+        // Version was a match, connection is verified.
+        return ConnectionState.Verified;
     }
 }
