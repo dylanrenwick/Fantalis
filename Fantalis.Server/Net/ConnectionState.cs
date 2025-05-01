5a3cb1bdc976858eeb002be28c6c4d9ca73dee52
Dylan Renwick <skidsirongenesisdev@gmail.com>
2025-05-01 15:40:16 -0400

    Removed unused ConnectionState file

--- Fantalis.Server/Net/ConnectionState.cs
+++
@@ -1,9 +0,0 @@
-﻿using Fantalis.Core.Net;
-
-namespace Fantalis.Server.Net;
-
-public class ConnectionState
-{
-    public required Connection Connection { get; init; }
-    public bool IsVerified { get; set; }
-}
